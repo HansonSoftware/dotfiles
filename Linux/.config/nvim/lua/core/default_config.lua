9834f563cfea49c3f16865b5ac4bfbc86b30ffbf
Hayden Hanson <hayden@ubuntu.mynetworksettings.com>
2024-01-03 23:04:12 -0500

    add linux dotfiles (early additions)

---
+++ Linux/.config/nvim/lua/core/default_config.lua
@@ -0,0 +1,92 @@
+local M = {}
+
+M.options = {
+  nvchad_branch = "v2.0",
+}
+
+M.ui = {
+  ------------------------------- base46 -------------------------------------
+  -- hl = highlights
+  hl_add = {},
+  hl_override = {},
+  changed_themes = {},
+  theme_toggle = { "onedark", "one_light" },
+  theme = "onedark", -- default theme
+  transparency = false,
+  lsp_semantic_tokens = false, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
+
+  -- https://github.com/NvChad/base46/tree/v2.0/lua/base46/extended_integrations
+  extended_integrations = {}, -- these aren't compiled by default, ex: "alpha", "notify"
+
+  -- cmp themeing
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "colored", -- colored / simple
+  },
+
+  telescope = { style = "borderless" }, -- borderless / bordered
+
+  ------------------------------- nvchad_ui modules -----------------------------
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    overriden_modules = nil,
+  },
+
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    show_numbers = false,
+    enabled = true,
+    lazyload = true,
+    overriden_modules = nil,
+  },
+
+  -- nvdash (dashboard)
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+      "                                                                                       ",
+      "                                                                                       ",
+      "   ██████╗██████╗  █████╗ ███████╗████████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗███████╗ ",
+      "  ██╔════╝██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝██╔════╝ ",
+      "  ██║     ██████╔╝███████║█████╗     ██║   ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ ███████╗ ",
+      "  ██║     ██╔══██╗██╔══██║██╔══╝     ██║   ██║███╗██║██║   ██║██╔══██╗██╔═██╗ ╚════██║ ",
+      "  ╚██████╗██║  ██║██║  ██║██║        ██║   ╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗███████║ ",
+      "   ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝        ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ",
+      "                                                                                       ",
+      "                                                                                       ",
+    },
+    buttons = {
+      { "⌕ Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
+  cheatsheet = { theme = "grid" }, -- simple/grid
+
+  lsp = {
+    -- show function signatures i.e args as you type
+    signature = {
+      disabled = false,
+      silent = true, -- silences 'no signature help available' message from appearing
+    },
+  },
+}
+
+M.plugins = "" -- path i.e "custom.plugins", so make custom/plugins.lua file
+
+M.lazy_nvim = require "plugins.configs.lazy_nvim" -- config for lazy.nvim startup options
+
+M.mappings = require "core.mappings"
+
+return M
---
+++ Linux/.config/nvim/lua/plugins/configs/treesitter.lua
@@ -0,0 +1,30 @@
+local options = {
+  ensure_installed = {
+    "json",
+    "javascript",
+    "typescript",
+    "python",
+    "tsx",
+    "yaml",
+    "html",
+    "css",
+    "markdown",
+    "markdown_inline",
+    "svelte",
+    "graphql",
+    "bash",
+    "lua",
+    "vim",
+    "dockerfile",
+    "gitignore",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
