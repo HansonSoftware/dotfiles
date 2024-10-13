5ee72b20c2178683484bc553ccbdb383a6c32c5d
Hayden <haydenhansondev+GitHub@gmail.com>
2024-10-13 12:56:55 -0400

    feat: set global statusline

--- .config/nvim/lua/hayden/core/opts.lua
+++ .config/nvim/lua/hayden/core/opts.lua
@@ -61,5 +61,8 @@ vim.opt.inccommand = "split"
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
+-- Set global status line
+vim.opt.laststatus = 3
+
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 8
