6caf615aefdbc83accd73252602d02cb662bb93f
Hayden <haydenhansondev+GitHub@gmail.com>
2025-06-17 09:07:49 -0400

    nvim: switch to rosepine theme
    - todo: update lsp config

--- .config/nvim/lua/hayden/plugins/lualine.lua
+++ .config/nvim/lua/hayden/plugins/lualine.lua
@@ -1,79 +1,11 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "ColorScheme",
 	config = function()
-		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-
-		local colors = {
-			blue = "#89b4fa",
-			green = "#a6e3a1",
-			violet = "#cba6f7",
-			yellow = "#f9e2af",
-			red = "#f38ba8",
-			fg = "#cdd6f4",
-			bg = nil,
-			fill_fg = "#1e1e2e",
-			inactive_bg = "#181825",
-		}
-
-		local my_lualine_theme = {
-			normal = {
-				a = { bg = colors.blue, fg = colors.fill_fg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			insert = {
-				a = { bg = colors.green, fg = colors.fill_fg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			visual = {
-				a = { bg = colors.violet, fg = colors.fill_fg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			command = {
-				a = { bg = colors.yellow, fg = colors.fill_fg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			replace = {
-				a = { bg = colors.red, fg = colors.fill_fg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			inactive = {
-				a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-				b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-				c = { bg = colors.inactive_bg, fg = colors.semilightgray },
-			},
-		}
-
-		-- configure lualine with modified theme
-		lualine.setup({
+		require("lualine").setup({
 			options = {
-				theme = my_lualine_theme,
-			},
-			extensions = {
-				"quickfix",
-				"nvim-tree",
-				"man",
-				"fzf",
-				"lazy",
-				"trouble",
-			},
-			sections = {
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#fab387" },
-					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
-				},
+				--- @usage 'rose-pine' | 'rose-pine-alt'
+				theme = "rose-pine",
 			},
 		})
 	end,
